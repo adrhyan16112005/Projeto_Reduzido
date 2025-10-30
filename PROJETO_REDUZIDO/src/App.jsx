ea05ed20f1b6584986ea2b33201021d2069c42e5
adrhyan16112005 <adrhyanviniciusnunesflores@gmail.com>
2025-10-30 15:46:19 -0400

    00
    000

--- PROJETO_REDUZIDO/src/App.jsx
+++ PROJETO_REDUZIDO/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import ConsultarMesas from './pages/ConsultarMesas';
-import ListarMinhasReservas from './pages/ListarMinhasReservas';
+import ConsultarMesas from './reservas/ConsultarMesas';
+import ListarMinhasReservas from './reservas/ListarMinhasReservas';
 
 function App() {
   return (
-    <div>
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Projeto Reservas de Mesas</h1>
+
+      {/* Página de Consulta de Mesas */}
       <ConsultarMesas />
+
       <hr />
+
+      {/* Página de Listagem de Reservas */}
       <ListarMinhasReservas />
     </div>
   );
